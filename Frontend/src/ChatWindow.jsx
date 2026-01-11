bf82a63263e0989bd5c8e3bb66ac92eaad0d8ee1
Shubham Fartade <shubdya01212@gmail.com>
2026-01-11 19:25:06 +0530

    frontend ready for deployment

--- Frontend/src/ChatWindow.jsx
+++ Frontend/src/ChatWindow.jsx
@@ -26,7 +26,7 @@ function ChatWindow() {
         };
 
         try {
-            const response = await fetch("http://localhost:8880/api/chat", options);
+            const response = await fetch("https://sigmagpt-d1mo.onrender.com/api/chat", options);
             const res = await response.json();
             console.log(res);
             setReply(res.reply);
